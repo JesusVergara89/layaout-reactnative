2b93db06677eee3d723c462ab65da7b7d5159f0b
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-07 20:54:50 -0600

    learnig about flex

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native'
+import Box from './components/Box'
 
 export default function App() {
+  const boxArray = [
+    { name: "box 1", color: "green" },
+    { name: "box 2", color: "red" },
+    { name: "box 3", color: "blue" },
+    { name: "box 4", color: "yellow" },
+    { name: "box 5", color: "purple" },
+    { name: "box 6", color: "orange" },
+    { name: "box 7", color: "pink" }
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {boxArray.map((data, i) => (
+        <Box key={i} children={data.name} style={{ backgroundColor: data.color, flex: 1}} />
+      ))}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 0.95,
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red"
+  }
+})
